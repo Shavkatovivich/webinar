da4d2d8f3d8256737853e29156c4fd5a99c8c0c5
shavkatovivich <frontend6774@gmail.com>
2023-03-12 20:48:00 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const headerExit = document.querySelector(".header__exit");
+headerExit.addEventListener("click", function() {
+    const headerWebinar = document.querySelector(".header__webinar");
+    headerWebinar.style.display = "none"
+})
